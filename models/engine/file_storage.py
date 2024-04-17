48681fcb0078071047b693002f518650c979a741
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2024-04-17 04:36:52 -0700

    add documentaion for delete method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,6 +20,7 @@ class FileStorage:
             return filterd_dict
 
     def delete(self, obj=None):
+        '''Removes an object from the storage dictionary.'''
         if obj is not None:
             obj_key = obj.to_dict()['__class__'] + '.' + obj.id
             if obj_key in self.__objects.keys():
